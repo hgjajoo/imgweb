a1b87b16c05acef03f2eb1acc082fa48d0b0a48e
Harshvardhan Jajoo <63530396+hgjajoo@users.noreply.github.com>
2024-02-02 16:34:24 +0530

    adding files

---
+++ readme.md
@@ -0,0 +1,48 @@
+# Image Generator
+
+Welcome to the Image Generator! Our web application is designed to make image generation a breeze, giving you the power to bring your ideas to life with just a few clicks.
+
+## Table of Contents
+
+- [Features](#features)
+- [How to Use](#how-to-use)
+- [Contributing](#contributing)
+- [Contributors](#contributors)
+
+## Features
+
+- **Dynamic Image Generation:** Allows users to generate images based on search queries.
+- **Responsive Design:** Supports both desktop and mobile devices.
+- **Image Zoom:** Provides a zoom effect on images when hovered.
+- **Overlay Display:** Shows a detailed overlay with the option to download images.
+
+## How to Use
+
+1. **Access the Image Generator Online:**
+
+   - Go to [Image Generator](https://imgweb.vercel.app/).
+
+2. **Generate Images:**
+
+   - Enter a word in the search bar.
+   - Press the search button to generate images.
+
+3. **Interact with the Image Generator:**
+
+   - Use the search bar to generate images.
+   - Click on an image to view it in an overlay.
+   - Download the image from the overlay.
+
+## Contributing
+
+If you'd like to contribute to the development of the Image Generator, follow these steps:
+
+1. Fork the repository.
+2. Create a new branch for your feature: `git checkout -b feature-name`.
+3. Commit your changes: `git commit -m 'Add some feature'`.
+4. Push to the branch: `git push origin feature-name`.
+5. Submit a pull request.
+
+## Contributors
+
+- [hgjajoo]()
---
+++ script.js
@@ -0,0 +1,96 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const wordElement = document.getElementById('word');
+    const imageContainer = document.getElementById('imageContainer');
+    const searchInput = document.getElementById('searchInput');
+    const searchButton = document.getElementById('searchButton');
+    const overlay = document.getElementById('overlay');
+    const overlayImage = document.getElementById('overlayImage');
+    const closeOverlayButton = document.getElementById('closeOverlay');
+    const downloadButton = document.getElementById('downloadButton');
+    let currentWord = ''; //set default word
+    let isDownloading = false;
+    let currentImageUrl = '';
+
+    fetchImages();
+
+    searchButton.addEventListener('click', function () {
+        currentWord = searchInput.value.trim();
+        fetchImages();
+    });
+
+    searchInput.addEventListener('keydown', function (event) {
+        if (event.key === 'Enter') {
+            searchButton.click();
+        }
+    });
+
+    async function fetchImages() {
+        try {
+            const apiKey = process.env.UNSPLASH_API_KEY;
+            const perPage = 20; // per page imgs
+            const response = await fetch(`https://api.unsplash.com/search/photos?query=${currentWord}&per_page=${perPage}&client_id=${apiKey}`, {
+                method: 'GET',
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            wordElement.textContent = currentWord;
+            displayImages(data.results);
+        } catch (error) {
+            console.error('Error fetching images:', error.message);
+        }
+    }
+
+    function displayImages(images) {
+        imageContainer.innerHTML = '';
+        images.forEach((imageData) => {
+            const imageElement = document.createElement('img');
+            imageElement.src = imageData.urls.small;
+            imageElement.alt = 'Unsplash Image';
+            imageElement.className = 'image';
+            imageContainer.appendChild(imageElement);
+            imageElement.addEventListener('click', function () {
+                openOverlay(imageData.urls.regular);
+                currentImageUrl = imageData.urls.regular;
+            });
+        });
+    }
+
+    function openOverlay(imageUrl) {
+        overlayImage.src = imageUrl;
+        overlay.style.display = 'flex';
+        closeOverlayButton.addEventListener('click', closeOverlay);
+        downloadButton.addEventListener('click', function downloadHandler() {
+            if (!isDownloading) {
+                isDownloading = true;
+                downloadImage(currentImageUrl);
+            }
+        });
+    }
+
+    function closeOverlay() {
+        overlay.style.display = 'none';
+        overlayImage.src = '';
+        isDownloading = false;
+        downloadButton.removeEventListener('click', downloadHandler);
+    }
+
+    function downloadImage(imageUrl) {
+        fetch(imageUrl)
+            .then(response => response.blob())
+            .then(blob => {
+                const link = document.createElement('a');
+                link.href = window.URL.createObjectURL(blob);
+                link.download = 'downloaded_image.jpg'; //change download name
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            })
+            .catch(error => console.error('Error fetching image:', error))
+            .finally(() => {
+                isDownloading = false;
+            });
+    }
+});
